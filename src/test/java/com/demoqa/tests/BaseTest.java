7d5cacdf9d6eade6da44127f561b7f3354b86b89
19427281 <1324CjrjkbysqU4231>
2022-09-12 23:35:16 +0300

    tests created

---
+++ src/test/java/com/demoqa/tests/BaseTest.java
@@ -0,0 +1,39 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import com.demoqa.pages.FormPage;
+import com.demoqa.pages.TestData;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BaseTest {
+
+    FormPage formPage = new FormPage();
+    TestData testData = new TestData();
+
+    @BeforeAll
+    static public void config() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
